8ae887aca712cdfe9da01a01597eed2f095b0ee2
OldNemesis <digital@tandemcreate.co.za>
2023-09-15 12:15:47 +0200

    add in trait in order to assign a role to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 // Get relationship classes
 use Illuminate\Database\Eloquent\Model;
@@ -17,7 +18,7 @@ use App\Models\UserInfo;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
