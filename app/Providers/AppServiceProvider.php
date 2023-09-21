40167892f75b24296b17745b7227a1b3059daced
OldNemesis <digital@tandemcreate.co.za>
2023-09-21 23:17:37 +0200

    built custom validator to check if valid SA ID or not with custom message if not

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+
+use Log;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,54 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('sa_id', function ($attribute, $value, $parameters, $validator) {
+           // Check if the ID number has the correct length (13 characters)
+            if (strlen($value) !== 13) {
+                return false;
+            }
+
+            $oddDigits = [];
+            $evenDigits = [];
+            $ids = str_split($value, 1);
+
+            Log::info($ids);
+
+            foreach ($ids as $index => $id) {
+                if($index % 2 == 1) {
+                    $evenDigits[] = $id;
+                } else {
+                    $oddDigits[] = $id;
+                }
+            }
+
+            $oddDigits = array_slice($oddDigits, 0, -1);
+
+            $oddSum = array_sum($oddDigits);
+            $evenSum = implode("",$evenDigits);
+            $evenSum = $evenSum * 2;
+            $evenSum = str_split($evenSum, 1);
+            $evenSum = array_sum($evenSum);
+
+            $combinedSum = $oddSum + $evenSum;
+
+            $combinedSumSplit = str_split($combinedSum, 1);
+
+            $compareDigit = array_slice($combinedSumSplit, 1);
+            $compareDigit = implode("",$compareDigit);
+            $compareDigit = 10 - $compareDigit;
+
+            
+            if($compareDigit == substr($value, -1)) {
+                Log::info($compareDigit == substr($value, -1));
+                return true;
+            }
+
+            return false;
+        });
+
+        Validator::replacer('sa_id', function ($message, $attribute, $rule, $parameters) {
+            // Customize the validation error message here
+            return "Please provide a valid SA ID";
+        });
     }
 }
