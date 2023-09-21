49f95da60be9ceb6eb238b52bb1e0ca1accd6664
OldNemesis <digital@tandemcreate.co.za>
2023-09-21 23:12:17 +0200

    Created the routes of the system, still needs to be cleaned up

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
+use App\Http\Controllers\UserActivationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,14 +28,25 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'view'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/activate-user/{user}-{token}', [UserActivationController::class, 'userActivation'])->name('user.activate')->middleware('admin.activate');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/edit-user/{user_id}', [DashboardController::class, "viewEditUser"])->name('list.users');
+    Route::put('/edit-user/{user_id}', [DashboardController::class, "updateUser"])->name('update.users');
+    Route::delete('/edit-user/{user_id}/delete', [DashboardController::class, "deleteUser"])->name('delete.users');
+
+    Route::get('/related-users', [DashboardController::class, 'view'])->name('list.related.users');
+    Route::get('/user-profile/{user_id}', [DashboardController::class, "viewUser"])->name('view.user');
+    Route::put('/user-profile/{user_id}', [DashboardController::class, "updateUser"])->name('update.user');
+    Route::delete('/user-profile/{user_id}/delete', [DashboardController::class, "deleteUser"])->name('delete.user');
 });
 
 require __DIR__.'/auth.php';
