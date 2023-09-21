6b0de12c5bf8db4f3613a70e5d14e87a4cb49f95
OldNemesis <digital@tandemcreate.co.za>
2023-09-21 23:09:26 +0200

    Also part of the activation process

---
+++ app/Http/Controllers/UserActivationController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Inertia\Inertia;
+use App\Models\User;
+use App\Events\AccountApporval;
+
+use Log;
+
+class UserActivationController extends Controller
+{
+    // userActivation
+    public function userActivation($user_id, $token) {
+        
+        $user = User::find($user_id);
+
+        $user->activated = 1;
+
+        $user_update = $user->save();
+
+        // $user_update = $user->update([
+        //     'activated' => 1
+        // ]);
+
+        try{
+            if($user_update) {
+                event(new AccountApporval($user));
+
+                return to_route('dashboard');
+            }
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+
+        $check_role = Auth::user()->hasRole('admin');
+        
+        return Inertia::render('Dashboard', [
+            'isAdmin' => $check_role,
+        ]);
+    }
+}
